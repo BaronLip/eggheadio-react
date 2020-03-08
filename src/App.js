76053231fd4d5664cbe32be6a046a8c7e6444da9
BaronLip <baronlip@gmail.com>
2020-03-07 22:48:09 -0600

    developed component and added notes on PropTypes and Default props.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Importing React from the 'react' package is required for all components.
 import React from 'react';
+import PropTypes from 'prop-types'
 
 // A class component may contain state:
 // The render() method is required for anything to be rendered on screen.
@@ -10,11 +11,46 @@ import React from 'react';
 
 class App extends React.Component {
 	render() {
-		return <h1 className="">Hello World!</h1>
-		// Testing the longhand:
-		// return React.createElement('h1', null, 'Hello World!');
+		// React components can only return one 'node'.
+		// To compensate for this, wrap multiple nodes in a parent node.
+		// JavaScript must be placed before the return statement, or be interpolated within the return statement.
+		//  
+		let txt = this.props.txt;
+		let cat = this.props.cat;
+		return (
+			<div>
+				<h1 className="">Hello World!</h1>
+				{/* return React.createElement('h1', null, 'Hello World!'); */}
+				{/* NOTE: JSX comments must be wrapped in braces. */}
+
+				{/* Import props by using 'this.props.propName'. */}
+				<h1>{this.props.txt}</h1>
+				{/* Or, set the prop to a variable outside of the render method. */}
+				<h1>{txt}</h1>
+				<h1>{cat}</h1>
+
+				{/* Testing the longhand:*/}
+			</div>
+		)
 	}
 }
+
+// Another way to add properties to a component is using a 'propTypes' object.
+// NOTE: propTypes is paired with React.PropTypes, with capital "P".
+// The key is the property name and the value is the 'type' of the value prefaced with React.propTypes.
+// propTypes is used to verify the datatype of the prop is a valid entry.
+// .isRequired, will throw a warning in the console if the prop is not available. The all will continue to work though.
+// NOTE: PropTypes now needs to be imported first before using. The video is a little out of date. 
+App.propTypes = {
+	txt: PropTypes.string,
+	cat: PropTypes.number.isRequired
+}
+
+// Set default props by using .defaultProps.
+App.defaultProps = {
+	txt: "this is the default txt.",
+}
+
 
 // A stateless component only returns the return block and has not state.
 // Example:
--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import App from './App';
 // import './index.css';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+{/*The 'text' attribute within the App tag below is a prop.
+Although this is index.js file, App is hard coded the "prop"erties of 'txt and 'cat'.
+In the App.js file, */}
+ReactDOM.render(
+    <App txt="this is the prop 'txt'" cat={5} />,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
